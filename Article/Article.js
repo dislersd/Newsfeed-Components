d6923a4f2a9dc5ad9656afe89abb8a40ae2843a5
dislersd <dislersd@gmail.com>
2019-01-30 12:55:19 -0800

    added expand/close text toggle on expand button

--- Article/Article.js
+++ Article/Article.js
@@ -13,6 +13,11 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+      if (this.expandButton.textContent == 'Expand') {
+    this.expandButton.textContent = 'Close';
+    } else {
+      this.expandButton.textContent = 'Expand'; 
+    }
   }
 }
 
